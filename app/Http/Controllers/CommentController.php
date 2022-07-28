ae87c7fd7f718002969b83fecc6cd9dd677977b4
Mo! <mhd.murad.87@gmail.com>
2022-07-28 18:19:41 +0300

    Prepare Backend
    - create for comment
    --model
    --factory
    --migration
    --controller
    --request

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreComment;
+
+class CommentController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+    public function store(StoreComment $request)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/StoreComment.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreComment extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'body' => 'required|string',
+            'parent_id' => 'nullable',
+            'level' => 'integer|between:0,2',
+            'post_id' => 'required|integer',
+        ];
+    }
+}
